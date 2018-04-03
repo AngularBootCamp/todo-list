d2cc55d5c3c53d9e7c84fca63b81cdf6d5763928
Paul Spears <paul.spears@oasisdigital.com>
2018-04-03 17:10:54 -0500

    remove deprecated HttpModule
    All lint rules pass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
@@ -18,9 +16,7 @@ import { TodoListStorageService } from './todo-list-storage.service';
     ListManagerComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule
   ],
   providers: [TodoListService, TodoListStorageService],
   bootstrap: [AppComponent]
