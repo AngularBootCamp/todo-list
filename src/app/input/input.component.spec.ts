60610224d424fecdb1c7612210dcb466f9761e6b
Paul Spears <paul.spears@oasisdigital.com>
2018-04-03 16:35:20 -0500

    removed unused imports

--- src/app/input/input.component.spec.ts
+++ src/app/input/input.component.spec.ts
@@ -1,7 +1,4 @@
-/* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { InputComponent } from './input.component';
 
@@ -13,7 +10,7 @@ describe('InputComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ InputComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
